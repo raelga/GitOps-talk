7ac5030a9779090efef56eb5bddb46ab6a1177d5
rael <me@rael.io>
2019-04-23 00:37:26 +0200

    Add gcp-wipe-helper script

---
+++ scripts/gcp-wipe-helper.sh
@@ -0,0 +1,8 @@
+# Helps wiping resources whom deletion is not supported by Deployment manager
+
+#
+# Unable to delete compute.v1.globalAddresses ranges with DM
+# https://issuetracker.google.com/issues/131072617
+#
+gcloud compute networks delete base-net
+gcloud deployment-manager deployments delete networks --delete-policy abandon
