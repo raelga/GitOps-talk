3eafdfad13bd6062e169ea679c752f21e25d0084
rael <me@rael.io>
2019-04-25 01:55:25 +0200

    Include provider cfg helper from providercfg.sh

--- helpers/infractl.sh
+++ helpers/infractl.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+source $(dirname $0)/providercfg.sh
+
 function error() { echo "[!] $@"; exit; }
 function info()  { echo "[i] $@"; }
 
@@ -25,39 +27,14 @@ function check() {
   test -f ${MANIFEST:-}   || error "MANIFEST file \"${MANIFEST:-}\" not found.";
 }
 
-function gcp_config() {
+function gcp_apply() {
 
-  which -s gcloud \
-    || error "gcloud SDK is not installed." \
-    && info "gcloud installed with $(gcloud version | grep 'Google Cloud SDK')."
-
-  test ! -z ${GCLOUD_SERVICE_ACCOUNT:-} \
-    || error "GCLOUD_SERVICE_ACCOUNT environment variable must be set." \
-    && {
-      gcloud auth activate-service-account \
-        --key-file <(echo ${GCLOUD_SERVICE_ACCOUNT} | base64 -D) \
-      || error "Unable to activate the service account." \
-      && info  "Service account set to $(gcloud config get-value account)."
-    }
-
-  test ! -z ${GCLOUD_PROJECT_ID:-} \
-    || error "GCLOUD_PROJECT_ID environment variable must be set." \
-    && info  "GCLOUD_PROJECT_ID project set to ${GCLOUD_PROJECT_ID}.";
-
-  gcloud config set project ${GCLOUD_PROJECT_ID} \
-    || error "Unable to set project ${GCLOUD_PROJECT_ID}." \
-    && info  "gcloud project set to $(gcloud config get-value project)."
+  gcp_config;
 
   DEPLOYMENT_CONF=${MANIFEST}
   DEPLOYMENT_FILE=$(basename ${DEPLOYMENT_CONF})
   DEPLOYMENT_PATH=$(dirname ${DEPLOYMENT_CONF})
   DEPLOYMENT_NAME=$(basename ${DEPLOYMENT_PATH})-${DEPLOYMENT_FILE%.*}
-
-}
-
-function gcp_apply() {
-
-  gcp_config;
 
   gcloud deployment-manager deployments describe ${DEPLOYMENT_NAME} \
     && info "Deployment ${DEPLOYMENT_NAME} already exists." \
